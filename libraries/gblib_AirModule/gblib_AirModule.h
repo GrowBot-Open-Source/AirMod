3c2170483d8bf04676eaf61f868affc7217a3296
MrcoSchrnr <MarcoSchreiner@web.de>
2024-03-20 00:24:21 +0100

    prepared airmod

---
+++ libraries/gblib_AirModule/gblib_AirModule.h
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#include <gblib_SD2405_RTCModule.h>
+#include <gb_SEN0546_TemperatureHumiditySensor.h>
+#include <gb_ENS160_AirQualitySensor.h>
+#include <gb_20X04_LEDDisplay.h>
+
